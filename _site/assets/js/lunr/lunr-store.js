6e905337b77f3de1ec98906f1b1de590e98acd08
kali <pmenner28@gmail.com>
2020-06-06 13:57:55 -0400

    init

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "TEST TEST",
+        "excerpt":"TEST  ","categories": ["Boxes"],
+        "tags": ["boxes"],
+        "url": "http://localhost:4000/blog/boxes/template/",
+        "teaser": null
+      },{
+        "title": "Another test",
+        "excerpt":"TEST Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit...","categories": ["Boxes"],
+        "tags": ["boxes"],
+        "url": "http://localhost:4000/blog/boxes/asd/",
+        "teaser": null
+      }]
