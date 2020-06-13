fcedb3d80bdd117144d1cf23297f681d5ba36ece
kali <pmenner28@gmail.com>
2020-06-13 04:42:23 -0400

    init

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -7,7 +7,13 @@ var store = [{
       },{
         "title": "Another test",
         "excerpt":"TEST Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit...","categories": ["Boxes"],
-        "tags": ["boxes"],
+        "tags": ["heap exploitation"],
         "url": "http://localhost:4000/blog/boxes/asd/",
+        "teaser": null
+      },{
+        "title": null,
+        "excerpt":"Overview PlayerTwo is MrR3boot’s follow-up box to his box Player - this time created in collaboration with b14ckh34rt. Recon A nmap -p- 10.10.10.170 reveals an interesting port 8545 besides the usual ssh and http ports. Not shown: 65532 closed ports PORT STATE SERVICE 22/tcp open ssh 80/tcp open http 8545/tcp...","categories": [],
+        "tags": null,
+        "url": "http://localhost:4000/blog/2020-04-30-player2/",
         "teaser": null
       }]
