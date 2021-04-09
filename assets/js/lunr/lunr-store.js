176abe5e8d93380d8bb1483ca84cbe6702d79dc4
kali <pmenner28@gmail.com>
2021-04-09 07:25:10 -0400

    test

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,49 +1,37 @@
----
-layout: null
----
-
-var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign teaser = site.teaser -%}
-      {%- endif -%}
-      {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }},
-        "teaser": {{ teaser | absolute_url | jsonify }}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}]
+var store = [{
+        "title": "Write-up: HackThebox - ServMon",
+        "excerpt":"Overview This is an interesting box as it has gotten a lot of heat - mostly due to the steps required for root - and I can see why: The web interface might be slowish and buggy under load but also people can mess with each other - pushing the...","categories": ["boxes"],
+        "tags": ["boxes","windows"],
+        "url": "http://localhost:4000/boxes/ServMon/",
+        "teaser": null
+      },{
+        "title": "Write-up: HackTheBox - PlayerTwo",
+        "excerpt":"Overview PlayerTwo is MrR3boot’s follow-up box to his box Player - this time created in collaboration with b14ckh34rt. It requires quite a bit of fuzzing and research on twirp to start the box, a little bit of binary exploitation for user and and even more so to get root without...","categories": ["boxes"],
+        "tags": ["heap","binary exploitation","linux"],
+        "url": "http://localhost:4000/boxes/player2/",
+        "teaser": null
+      },{
+        "title": "Write-up: HackTheBox - Travel",
+        "excerpt":"Starting the travel - Overview Travel is an interesting box created by jkr and his team member xct. It required some fuzzing and enumerating to get a foothold, exploiting memcached abusing a SSRF vulnerability and finally modifying users by taking advantage of our LDAP privileges. Recon Since the nmap scan...","categories": ["boxes"],
+        "tags": ["memcached","docker","ldap"],
+        "url": "http://localhost:4000/boxes/travel/",
+        "teaser": null
+      },{
+        "title": "Write-up: HackThebox - RopMe",
+        "excerpt":"Overview Running the binary just asks us to ROP me outside, how 'about dah? which hints at return oriented programming (ROP) - redirecting code execution by abusing a buffer overflow vulnerability. And indeed, when hammering the input with a lot of characters we get a seg fault: $ ./ropme ROP...","categories": ["challenges"],
+        "tags": ["ROP","pwn"],
+        "url": "http://localhost:4000/challenges/RopMe/",
+        "teaser": null
+      },{
+        "title": "Write-up: HackTheBox - Time",
+        "excerpt":"Overview This is a brief walkthrough for the box Time from HackTheBox with some remarks on file/directory permissions in the appendix. Getting a shell on the box, user flag As only port 22 and 80 are open we take a look at the website and find an online beautifier and...","categories": ["boxes"],
+        "tags": ["linux"],
+        "url": "http://localhost:4000/boxes/Time/",
+        "teaser": null
+      },{
+        "title": "Writeup: HackTheBox - Schooled",
+        "excerpt":"Overview home page: mentioning moodle hostname admissions@schooled.htb phillips_manuel@staff.schooled.htb We find the moodle subdomain by fuzzing: $ ffuf -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt -u http://schooled.htb -H \"Host: FUZZ.schooled.htb\" -fw 5338 [snip] moodle [Status: 200, Size: 84, Words: 5, Lines: 2] :: Progress: [4989/4989] :: Job [1/1] :: 451 req/sec :: Duration: [0:00:15] :: Errors:...","categories": ["boxes"],
+        "tags": ["freebsd","bsd","moodle","pkg"],
+        "url": "http://localhost:4000/boxes/Schooled/",
+        "teaser": null
+      }]
